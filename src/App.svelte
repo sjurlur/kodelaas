12863a5c87a25ac8b63c47b843d2bb04dbf2dbee
Sjur Seibt <sjur.seibt@nrk.no>
2021-10-04 13:44:05 +0200

    app

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,138 @@
 <script>
-	export let name;
+  import { fade } from "svelte/transition";
+  const buttons = [1, 2, 3, 4, 5, 6, 7, 8, 9, "𐄂", 0, "✓"];
+  let userInput = "";
+  let code = "7654";
+
+  let error = false;
+  let solved = false;
+
+  function click(value) {
+    error = false;
+    requestAnimationFrame(() => {});
+    if (value === "𐄂") {
+      clear();
+    } else if (value === "✓") {
+      submit(userInput);
+    } else {
+      userInput += value;
+    }
+  }
+
+  function clear() {
+    userInput = "";
+  }
+
+  function submit(userInput) {
+    if (userInput === code) {
+      solved = true;
+    } else {
+      error = true;
+      setTimeout(() => {
+        clear();
+        error = false;
+      }, 500);
+    }
+  }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  {#if !solved}
+    <div out:fade>
+      <code>{userInput}</code>
+      <div class="buttons">
+        {#each buttons as button, i}
+          {#if i === buttons.length - 1}
+            <button
+              class:error
+              class:solved
+              class="button"
+              on:click={() => {
+                click(button);
+              }}>{button}</button
+            >
+          {:else}
+            <button
+              class="button"
+              on:click={() => {
+                click(button);
+              }}>{button}</button
+            >
+          {/if}
+        {/each}
+      </div>
+    </div>
+  {:else}
+    <div class="answer" in:fade={{ delay: 350 }}>
+      <h2>Hipp hipp hurra! 🎉</h2>
+      <p>Neste hint er i en maskin som har samme initialer som Hans Petter</p>
+    </div>
+  {/if}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  code {
+    flex: 1 1 auto;
+    display: block;
+    margin-bottom: 1rem;
+    min-height: 27px;
+    font-size: 1rem;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .buttons {
+    display: grid;
+    gap: 20px;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+  }
+
+  .button {
+    padding: 0.5rem 0.75rem;
+    font-size: 1.5rem;
+    font-weight: 500;
+    background-color: rgba(233, 233, 237, 1);
+  }
+
+  .error {
+    animation: fadered 0.2s forwards ease-out;
+    animation-delay: 0.3s;
+    background-color: rgba(169, 0, 0, 1);
+  }
+
+  .solved {
+    animation: fadered 0.3s forwards ease-out;
+    animation-delay: 1s;
+    background-color: rgba(0, 169, 0, 1);
+  }
+
+  @keyframes fadered {
+    from {
+      background-color: rgba(169, 0, 0, 1);
+    }
+    to {
+      background-color: rgba(233, 233, 237, 1);
+    }
+  }
+  @keyframes fadegreen {
+    from {
+      background-color: rgba(0, 169, 0, 1);
+    }
+    to {
+      background-color: rgba(233, 233, 237, 1);
+    }
+  }
+
+  .answer {
+    max-width: 400px;
+    font-size: 2rem;
+  }
 </style>
